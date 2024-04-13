a5d96087930184add072448d6bbdebe91a2bb9c4
Michael3194 <143271230+Michael3194@users.noreply.github.com>
2024-04-13 01:01:38 -0230

    remove commented code

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,73 +1,3 @@
-// import { useState } from 'react';
-// import { Link, useNavigate } from 'react-router-dom';
-// import { useAuth } from '../auth/AuthContext';
-
-// const LoginPage = () => {
-//   const [username, setUsername] = useState('');
-//   const [password, setPassword] = useState('');
-//   const [error, setError] = useState('');
-//   const navigate = useNavigate();
-//   const { login } = useAuth(); // Access the login function from AuthContext
-
-//   const handleSubmit = async (event) => {
-//     event.preventDefault();
-
-//     try {
-//       console.log('Logging in...');
-//       await login(username, password);
-//       navigate('/admin'); // Assume successful login redirects to /admin
-//     } catch (error) {
-//       console.error('Error:', error);
-//       setError('Login failed - Please try again');
-//     }
-//   };
-
-//   return (
-//     <div className='page'>
-//       <div className='login-form'>
-//         <h2>Login</h2>
-//         <form onSubmit={handleSubmit}>
-//           <div>
-//             <label htmlFor='username'>Username:</label>
-//             <input
-//               type='text'
-//               id='username'
-//               name='username'
-//               value={username}
-//               onChange={(e) => setUsername(e.target.value)}
-//               required
-//             />
-//           </div>
-//           <div>
-//             <label htmlFor='password'>Password:</label>
-//             <input
-//               type='password'
-//               id='password'
-//               name='password'
-//               value={password}
-//               onChange={(e) => setPassword(e.target.value)}
-//               required
-//             />
-//           </div>
-//           <button
-//             type='submit'
-//             className='btn login-btn'
-//           >
-//             Login
-//           </button>
-//           {error && <p className='error'>{error}</p>}
-//         </form>
-//       </div>
-//       <h2>Don't have an account?</h2>
-//       <h3>
-//         <Link to='/signup'>Sign Up</Link>
-//       </h3>
-//     </div>
-//   );
-// };
-
-// export default LoginPage;
-
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../auth/AuthContext';
@@ -92,7 +22,6 @@ const LoginPage = () => {
       } else if (user && user.role === 'NORMAL') {
         navigate('/user');
       } else {
-        // Handle other scenarios if needed
         setError('Invalid user role');
       }
     } catch (error) {
